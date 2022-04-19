272d73000da7b98e80fc6e2fc1aec1713a2269b7
Kazi Mamun <kazimamun716@gmail.com>
2022-04-19 11:44:29 +0600

    modal added

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,10 +29,37 @@ const showData = (data) => {
                     <div class="card-body">
                         <h5 class="card-title">${mobile.brand}</h5>
                         <p class="card-text">${mobile.phone_name}</p>
+                        <button class="btn btn-primary" onclick="moreInfo('${mobile.slug}')" data-bs-toggle="modal" data-bs-target="#exampleModal">More Info</button>
                     </div>
                 </div>
             `;
             displayData.appendChild(div);
         })
     }
+};
+
+const moreInfo = (id)=>{
+    fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    .then(res => res.json())
+    .then(result => displayMobile(result))
+};
+
+const displayMobile = (mobile) =>{
+    console.log(mobile)
+    const div = document.createElement("div");
+    div.classList = "modal-content";
+    div.innerHTML = `
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            ...
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+    `
+    document.getElementById("display-mobile").appendChild(div);
 }
