9d3e661e5b22c44689158c8ef710801fb7dd1d53
Kazi Mamun <kazimamun716@gmail.com>
2022-04-19 16:10:09 +0600

    for submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,16 @@ document.getElementById("search-btn").addEventListener("click",()=>{
     }
 });
 
+//search mobile
 const searchMobile = (searchText) => {
     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     .then(result=> result.json())
-    .then(data=>showData(data))
+    .then(data=>{
+        showData(data)
+    })
 };
 
+//show searching data
 const showData = (data) => {
     if(!data.status){
         document.getElementById("display-result").innerHTML="";
@@ -24,8 +28,8 @@ const showData = (data) => {
         document.getElementById("text").style.display = "none";
         document.getElementById("mobile-not-found").style.display="none";
         const displayData = document.getElementById("display-result");
-        displayData.innerHTML="";
-        data.data.map(mobile=>{
+        displayData.innerHTML="";     
+        data.data.slice(0,20).map(mobile=>{
             const div = document.createElement("div");
             div.classList = "col col-md-4";
             div.innerHTML = `
@@ -39,7 +43,7 @@ const showData = (data) => {
                 </div>
             `;
             displayData.appendChild(div);
-        })
+        });
     }
 };
 
@@ -49,23 +53,23 @@ const moreInfo = (id)=>{
     .then(result => displayMobile(result))
 };
 
+//display single mobile info
 const displayMobile = (mobile) =>{
-    console.log(mobile)
     if(!mobile.status){
         console.log("no data found");
     } else {
         const displayMobile = document.getElementById("display-mobile");
-        const {storage, displaySize, chipSet, memory} = mobile.data.mainFeatures; 
+        const {storage, displaySize, chipSet, memory, sensors} = mobile.data.mainFeatures; 
         displayMobile.innerHTML = `
             <div class="modal-header text-center">
                 <h5 class="modal-title" id="mobileModalLabel">${mobile.data.name}</h5>
             </div>
-            <div class="modal-body text-center">
+            <div class="modal-body text-center" id="modal-body">
                 <div><img src="${mobile.data.image}" alt="${mobile.data.name}"/></div>
                 <p>Releasing: ${mobile.data.releaseDate? mobile.data.releaseDate : "Release Date Not Found"}</p>
-                <div id="features">
-                    <h4>Mobile Features:</h4>
-                    <div class="d-flex flex-column align-items-start border border-secondary ps-2">
+                <div class="features">
+                    <h4>Features:</h4>
+                    <div class="d-flex flex-column align-items-start border rounded border-secondary ps-2">
                         <p>Memory: ${memory}</p>
                         <p>Display: ${displaySize}</p>
                         <p>Storage: ${storage}</p>
@@ -73,10 +77,30 @@ const displayMobile = (mobile) =>{
                     </div>
                 </div>
             </div>
+            <ol id="sensor">
+            <h4 class="text-center">Sensors:</h4>
+            </ol>
+            <div>
+            <h4 class="text-center">Others:</h4>
+                <ol>
+                    <li>Wlan: ${mobile.data.others.WLAN}</li>
+                    <li>Bluethooth: ${mobile.data.others.Bluetooth}</li>
+                    <li>GPS: ${mobile.data.others.GPS}</li>
+                    <li>NFC: ${mobile.data.others.NFC}</li>
+                    <li>Radio: ${mobile.data.others.Radio}</li>
+                    <li>USB: ${mobile.data.others.USB}</li>
+                </ol>
+            </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
             </div>
-        `
+        `;
+        //sensor showing
+        sensors.length && sensors.forEach(sensor => {
+            const li = document.createElement("li");
+            li.innerText = sensor;
+            document.getElementById("sensor").appendChild(li);
+        });
     }
 };
 
